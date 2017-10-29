adc8d9f96aa0ba81758e1de635fd8ff429a407b7
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-10-29 23:20:54 +0900

    [add]UserRequest

---
+++ lime/Data/Network/Request/UserRequest.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  lime
+//
+//  Created by 下村一将 on 2017/10/29.
+//  Copyright © 2017 kazu. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+extension LimeAPI {
+	struct UserRequest: LimeAPIRequest {
+		let id: Int
+	}
+}
+
+extension LimeAPI.UserRequest {
+	typealias Response = FriendEntity
+	
+	var path: String {
+		return "/users"
+	}
+	
+	var method: HTTPMethod {
+		return .post
+	}
+	
+	var headers: HTTPHeaders? {
+		return nil
+	}
+	
+	var parameters: Parameters? {
+		return [
+			"id": id
+		]
+	}
+}
