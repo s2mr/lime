9d12a3684bae17635b9558260d3f5a0e4495376b
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-10-29 23:45:41 +0900

    [add]API init

---
+++ lime/Data/Network/API.swift
@@ -0,0 +1,41 @@
+//
+//  API.swift
+//  lime
+//
+//  Created by 下村一将 on 2017/10/29.
+//  Copyright © 2017 kazu. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol API {
+	var sessionConfiguration: URLSessionConfiguration { get }
+	var manager: SessionManager { get }
+	var baseURL: URL { get }
+}
+
+protocol APIRespondable {
+	associatedtype Response
+	func response(from: Any?) throws -> Response
+}
+
+protocol APIRequest: APIRespondable {
+	var path: String { get }
+	var method: HTTPMethod { get }
+	var headers: HTTPHeaders? { get }
+	var parameters: Parameters? { get }
+	var encoding: ParameterEncoding { get }
+	func validate(statusCode: Int) -> Bool
+}
+
+extension APIRequest {
+	var encoding: ParameterEncoding {
+		return URLEncoding.methodDependent
+	}
+	
+	public func validate(statusCode: Int) -> Bool {
+		return (200..<300).contains(statusCode)
+	}
+}
+
