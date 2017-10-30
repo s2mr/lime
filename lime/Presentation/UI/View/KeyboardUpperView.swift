d675ad0cd3586e0acc65815740ed8bd6991b1ffe
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-10-30 22:34:57 +0900

    [add]KeyboardUpperView

---
+++ lime/Presentation/UI/View/KeyboardUpperView.swift
@@ -0,0 +1,24 @@
+//
+//  KeyboardUpperView.swift
+//  lime
+//
+//  Created by 下村一将 on 2017/10/30.
+//  Copyright © 2017 kazu. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardUpperView: UIView {
+	
+	@IBOutlet weak var chatTextField: UITextField!
+	@IBOutlet weak var sendButton: UIButton!
+	
+	/*
+	// Only override draw() if you perform custom drawing.
+	// An empty implementation adversely affects performance during animation.
+	override func draw(_ rect: CGRect) {
+	// Drawing code
+	}
+	*/
+	
+}
